d8988366af759178b75620bf2976a706d623cd7f
satyen mori <satyenmori@gmail.com>
2024-04-29 12:38:13 +0530

    News And Events Page Changes Completed

--- src/Components/NewsandEvents.jsx
+++ src/Components/NewsandEvents.jsx
@@ -18,10 +18,12 @@ import React, { useEffect, useState } from "react";
 import { BiEditAlt } from "react-icons/bi";
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
 import { useNavigate } from "react-router-dom";
+import DeleteBtn from "./DeleteBtn";
 
 const NewsAndEvents = () => {
   const [category, setCategory] = useState([]);
   const [newsAndEvents, setNewsAndEvents] = useState([]);
+  const [searchNewsAndEvnts, setSearchNewsAndEvents] = useState([]);
   const [flag, setFlag] = useState(false);
   const [search, setSearch] = useState("");
   const [page, setPage] = useState(1);
@@ -30,7 +32,27 @@ const NewsAndEvents = () => {
 
   const navigate = useNavigate();
   const url = process.env.REACT_APP_DEV_URL;
-  const handleSearch = () => {};
+
+  const handleSearchChange = (e) => {
+    setSearch(e.target.value);
+    setFlag(false);
+  };
+
+  const handleSearch = () => {
+    if (!search.trim()) {
+      setFlag(true);
+      setCount(newsAndEvents.length);
+    }
+    const filteredData = newsAndEvents.filter((item) =>
+      item.generalheading.toLowerCase().includes(search.toLowerCase())
+    );
+    setSearchNewsAndEvents(filteredData);
+    setCount(filteredData.length)
+  };
+
+  useEffect(() => {
+    handleSearch();
+  }, [search, newsAndEvents]);
 
   const getCategory = async () => {
     try {
@@ -46,7 +68,7 @@ const NewsAndEvents = () => {
   const getNewsAndEvents = async (page) => {
     try {
       let res = await fetch(
-        `http://localhost:8080/newsandevent?page=${page}&limit=5`
+        `http://localhost:8080/newsandevent?page=${page}&limit=5&search=${search}`
       );
       const data = await res.json();
 
@@ -96,11 +118,15 @@ const NewsAndEvents = () => {
           <span>Search:</span>
           <Input
             color={"black"}
-            onBlur={() => setFlag(true)}
+            onFocus={() => setFlag(false)}
+            onBlur={() => {
+              if (!search.trim()) {
+                setFlag(true);
+              }
+            }}
             w="150px"
-            onChange={(e) => setSearch(e.target.value)}
+            onChange={handleSearchChange}
             value={search}
-            onKeyUp={handleSearch}
           />
         </Box>
         {/* <Button border={"1px solid #cfcccc"} rightIcon={<DeleteIcon />}>
@@ -121,14 +147,14 @@ const NewsAndEvents = () => {
             <Tr>
               <Th color={"#add8e6"}>#</Th>
               <Th color={"#add8e6"}>Heading</Th>
-              <Th color={"#add8e6"}>Description</Th>
               <Th color={"#add8e6"}>Date</Th>
               <Th color={"#add8e6"}>Place</Th>
               <Th color={"#add8e6"}>Action</Th>
             </Tr>
           </Thead>
           <Tbody>
-            {newsAndEvents?.map((item, index) => {
+            {searchNewsAndEvnts?.map((item, index) => {
+              const serialNumber = (page - 1) * 5 + index + 1;
               const formattedDate = new Date(item.date).toLocaleDateString(
                 "en-GB",
                 {
@@ -139,9 +165,8 @@ const NewsAndEvents = () => {
               );
               return (
                 <Tr key={item._id}>
-                  <Td> {(page - 1) * 5 + index + 1} </Td>
+                  <Td> {serialNumber} </Td>
                   <Td>{item?.generalheading}</Td>
-                  <Td>{item?.generaltext}</Td>
                   <Td>{formattedDate}</Td>
                   <Td>{item?.place}</Td>
                   <Td>
@@ -169,13 +194,8 @@ const NewsAndEvents = () => {
                       >
                         Edit
                       </Button>
-                      <Button
-                        leftIcon={<DeleteIcon />}
-                        bgColor={"red.400"}
-                        _hover={{ bgColor: "red.500", color: "white" }}
-                        color="white"
-                        onClick={() => handleDelete(item._id)}
-                      ></Button>
+
+                      <DeleteBtn handleDelete={() => handleDelete(item._id)} />
                     </ButtonGroup>
                   </Td>
                 </Tr>
--- src/Components/ViewNewsAndEvents.jsx
+++ src/Components/ViewNewsAndEvents.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import {
   Box,
   FormControl,
@@ -7,13 +7,19 @@ import {
   Input,
   Button,
   Center,
-  Textarea,
   Flex,
+  Text,
+  Textarea,
+  Image,
+  SimpleGrid,
 } from "@chakra-ui/react";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import { BiEditAlt } from "react-icons/bi";
 
 const ViewNewsAndEvents = () => {
   const { id } = useParams();
   const [item, setItem] = useState([]);
+  const navigate = useNavigate();
 
   const fetchEventAndNewsById = async () => {
     try {
@@ -28,160 +34,194 @@ const ViewNewsAndEvents = () => {
   useEffect(() => {
     fetchEventAndNewsById();
   }, [id]);
+
+  const handleDelete = async (id) => {
+    try {
+      const res = await fetch(`http://localhost:8080/newsandevent/${id}`, {
+        method: "DELETE",
+      });
+      alert("Data Delete Successfuly");
+      navigate("/admin/newsandevents");
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <>
-      <Center h="auto">
-        {" "}
-        {/* This centers the form vertically and horizontally */}
+      <Box textAlign={"left"} p="5">
+        <Flex gap="20px">
+          <Text fontSize={"xl"} fontWeight={"semibold"}>
+            View News & Events Details
+          </Text>
+          <Button
+            borderRadius={"20px"}
+            color={"#add8e6"}
+            bgColor={"black"}
+            _hover={{ color: "black", bgColor: "#add8e6" }}
+            leftIcon={<BiEditAlt />}
+            onClick={() => navigate(`/admin/newsandevents/edit/${id}`)}
+          >
+            Edit
+          </Button>
+          <Button
+            borderRadius={"20px"}
+            color={"#add8e6"}
+            bgColor={"black"}
+            _hover={{ color: "black", bgColor: "#add8e6" }}
+            leftIcon={<RiDeleteBin6Line />}
+            onClick={() => handleDelete(id)}
+          >
+            Delete
+          </Button>
+        </Flex>
+        <br />
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Hading
+        </Text>
         <Box
-          p={4}
-          boxShadow="md"
-          borderRadius="lg"
-          w="full"
-          maxW="3xl"
-          bg="white"
-          mt={10}
-          mb={8}
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          fontSize={"medium"}
+          _readOnly
         >
-          <form encType="multipart/form-data">
-            <FormControl>
-              <FormLabel htmlFor="generalheading">Heading</FormLabel>
-              <Input
-                id="generalheading"
-                type="text"
-                placeholder="Enter your Heading"
-                name="generalheading"
-                value={item.generalheading}
-                mb={4}
-                readOnly
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="generaltext">Description</FormLabel>
-              <Textarea
-                id="generaltext"
-                placeholder="Enter your message"
-                mb={4}
-                name="generaltext"
-                value={item.generaltext}
-                readOnly
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="detailimages">Images</FormLabel>
-              <img
-                src={`http://localhost:8080/newsAndevents/${item.cardimage}`}
-                alt="selected img"
-                style={{
-                  width: "100px",
-                  height: "100px",
-                  margin: "5px",
-                }}
-              />
-            </FormControl>
-
-            <FormControl>
-              <FormLabel htmlFor="cardheading">Card Heading</FormLabel>
-              <Input
-                id="cardheading"
-                type="text"
-                placeholder="Enter your Heading"
-                mb={4}
-                name="cardheading"
-                value={item.cardheading}
-                readOnly
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="cardtext">Card Description</FormLabel>
-              <Textarea
-                id="cardtext"
-                placeholder="Enter your Description"
-                mb={4}
-                name="cardtext"
-                value={item.cardtext}
-                readOnly
-              />
-            </FormControl>
-            <Flex gap={4} mb={4}>
-              <FormControl flex="1">
-                <FormLabel htmlFor="date">Date</FormLabel>
-                <Input
-                  id="date"
-                  type="text"
-                  name="date"
-                  value={item.date}
-                  readOnly
-                />
-              </FormControl>
-              <FormControl flex="1">
-                <FormLabel htmlFor="place">Place</FormLabel>
-                <Input
-                  id="place"
-                  type="text"
-                  placeholder="Enter your place"
-                  name="place"
-                  value={item.place}
-                  readOnly
-                />
-              </FormControl>
-              <FormControl flex="1">
-                <FormLabel htmlFor="detailheading">Detail Heading</FormLabel>
-                <Input
-                  id="detailheading"
-                  type="text"
-                  placeholder="Enter your Heading"
-                  name="detailheading"
-                  value={item.detailheading}
-                  readOnly
-                />
-              </FormControl>
-            </Flex>
-
-            <FormControl>
-              <FormLabel htmlFor="detailtext">Detail Description</FormLabel>
-              <Textarea
-                id="detailtext"
-                placeholder="Enter your Description"
-                mb={4}
-                name="detailtext"
-                value={item.detailtext}
-                readOnly
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="video">Video</FormLabel>
-              <Input
-                id="video"
-                type="text"
-                placeholder="Enter your video Link"
-                mb={4}
-                name="video"
-                value={item.video}
-                readOnly
-              />
-            </FormControl>
-            <FormControl>
-              <FormLabel htmlFor="detailimages">Activity Images</FormLabel>
-              <Flex wrap="wrap">
-                {item.detailimages &&
-                  item.detailimages.map((image, index) => (
-                    <img
-                      key={index}
-                      src={`http://localhost:8080/newsAndevents/${image}`}
-                      alt={`Image ${index}`}
-                      style={{
-                        width: "100px",
-                        height: "100px",
-                        margin: "5px",
-                      }}
-                    />
-                  ))}
-              </Flex>
-            </FormControl>
-          </form>
+          {item?.generalheading}
         </Box>
-      </Center>
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Description
+        </Text>
+        <Textarea
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          value={item?.generaltext}
+          fontSize={"medium"}
+          _readOnly
+        />
+        <br />
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Image
+        </Text>
+        <SimpleGrid columns={[1, 1, 1, 2, 2]} rowGap={"9"}>
+          <Image
+            src={`http://localhost:8080/newsAndevents/${item.cardimage}`}
+            style={{
+              width: "150px",
+              height: "100px",
+              margin: "5px",
+              marginLeft: "25px",
+            }}
+          />
+        </SimpleGrid>
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Card Hading
+        </Text>
+        <Box
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          fontSize={"medium"}
+          _readOnly
+        >
+          {item?.cardheading}
+        </Box>
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Card Description
+        </Text>
+        <Textarea
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          value={item?.cardtext}
+          fontSize={"medium"}
+          _readOnly
+        />
+        <br />
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Date
+        </Text>
+        <Box
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          fontSize={"medium"}
+        >
+          {item?.date}
+        </Box>
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Place
+        </Text>
+        <Box
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          fontSize={"medium"}
+        >
+          {item?.place}
+        </Box>
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Details Hading
+        </Text>
+        <Box
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          fontSize={"medium"}
+        >
+          {item?.detailheading}
+        </Box>
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Detail Description
+        </Text>
+        <Textarea
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          value={item?.detailtext}
+          fontSize={"medium"}
+        />
+        <br />
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Vidio
+        </Text>
+        <Box
+          padding="10px 20px"
+          width="50%"
+          bgColor={"#eef1f4"}
+          fontSize={"medium"}
+        >
+          {item?.video}
+        </Box>
+        <br />
+        <Text fontWeight={"semibold"} fontSize={"xl"}>
+          Activity Images
+        </Text>
+        <Box display="flex" flexDirection="row" flexWrap="wrap" width="50%">
+          {item?.detailimages &&
+            item?.detailimages.map((e, index) => (
+              <Box key={index} marginRight="7px">
+                <Image
+                  src={`http://localhost:8080/newsAndevents/${e}`}
+                  style={{
+                    width: "100px",
+                    height: "100px",
+                    margin: "5px",
+                  }}
+                />
+              </Box>
+            ))}
+        </Box>
+      </Box>
     </>
   );
 };
